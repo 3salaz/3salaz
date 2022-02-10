0c9fdd88d04f5ca0ff7f30830a6465811685fc5e
E <work@Es-MacBook-Pro.hsd1.ca.comcast.net>
2022-02-09 17:28:00 -0800

    bootstrap design

---
+++ public/scripts/main.js
@@ -0,0 +1,24 @@
+
+
+// // FIREBASE CONFIGURATION
+// var firebaseConfig = {
+//     apiKey: "AIzaSyAHPSqoB1SSgOKLo5Lh1oIOyrB9hCACX2I",
+//     authDomain: "salaz-b4309.firebaseapp.com",
+//     databaseURL: "https://salaz-b4309-default-rtdb.firebaseio.com",
+//     projectId: "salaz-b4309",
+//     storageBucket: "salaz-b4309.appspot.com",
+//     messagingSenderId: "775356987669",
+//     appId: "1:775356987669:web:ca605fc7e5a20fbf1f911c",
+//     measurementId: "G-RXJ83LGVQ5"
+//   };
+//   const app = initializeApp(firebaseConfig);
+//   const db = getFirestore(app);
+//   async function getContent(db) {
+//     const contentCol = collection(db, 'content');
+//     const contentSnapshot = await getDocs(citiesCol);
+//     const contentList = citySnapshot.docs.map(doc => doc.data());
+//     return contentList;
+//   }
+    
+
+// console.log(contentList);
